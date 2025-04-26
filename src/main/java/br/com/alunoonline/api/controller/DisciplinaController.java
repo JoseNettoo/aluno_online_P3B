e68aefe9b1c44b7a41ad8ae1ba68d1f1d1836041
José Neto <josepereiraneto1995@gmail.com>
2025-04-26 19:34:36 -0300

    Disciplina Finalizada

--- src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
+++ src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/disciplinas")
 public class DisciplinaController {
@@ -16,5 +19,27 @@ public class DisciplinaController {
     @PostMapping
     public void criarDisciplina(@RequestBody Disciplina disciplina){
         disciplinaService.criarDisciplina(disciplina);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Disciplina> listarTodasDisciplinas(){
+        return disciplinaService.listarTodasDisciplinas();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Disciplina> buscarDisciplinaPorId(@PathVariable Long id){
+        return disciplinaService.buscarDisciplinaPorId(id);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarDisciplinaPorId(@PathVariable Long id){
+        disciplinaService.deletarDisciplinaPorId(id);
+    }
+
+    public void atualizarDisciplinaPorId(@PathVariable Long id, @RequestBody Disciplina disciplina){
+        disciplinaService.atualizarDisciplinaPorId(id,disciplina);
     }
 }
