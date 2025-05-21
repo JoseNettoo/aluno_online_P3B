82633116742e74e4c327e36aef6099fe124e338f
José Neto <josepereiraneto1995@gmail.com>
2025-05-20 21:30:54 -0300

    atualizado 20-5

--- src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -1,6 +1,8 @@
 package br.com.alunoonline.api.service;
 
 import br.com.alunoonline.api.dtos.AtualizarNotasRequestDTO;
+import br.com.alunoonline.api.dtos.DisciplinasAlunoResponseDTO;
+import br.com.alunoonline.api.dtos.HistoricoAlunoResponseDTO;
 import br.com.alunoonline.api.enums.MatriculaAlunoStatusEnum;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.repository.MatriculaAlunoRepository;
@@ -86,6 +88,25 @@ public class MatriculaAlunoService {
         matriculaAlunoRepository.save(matriculaAluno);
 
     }
+    public HistoricoAlunoResponseDTO emitirHistorico (Long alunoId){
+        List<MatriculaAluno> matriculaAlunos = matriculaAlunoRepository.findByAlunoId(alunoId);
+        if (matriculaAlunos.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Esse aluno nao possui matricula");
+        }
+        HistoricoAlunoResponseDTO historicoAluno=new HistoricoAlunoResponseDTO();
+        historicoAluno.setNomeAluno(matriculaAlunos.get(0).getAluno().getNome());
+        historicoAluno.setEmailAluno(matriculaAlunos.get(0).getAluno().getEmail());
+        historicoAluno.setCpfAluno(matriculaAlunos.get(0).getAluno().getCpf());
+
+        List<DisciplinasAlunoResponseDTO> disciplinas = matriculaAlunos.stream()
+                .map(this :: mapearParaDisciplinasAlunoResponseDTO)
+                .toList();
+        historicoAluno.setDisciplinas(disciplinas);
+        return historicoAluno;
+
+    }
+
     private MatriculaAluno buscarMatriculaOuLancarExcecao(Long matriculaAlunoId){
         return matriculaAlunoRepository.findById(matriculaAlunoId)
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,
@@ -101,6 +122,19 @@ public class MatriculaAlunoService {
         }
 
     }
+    private DisciplinasAlunoResponseDTO mapearParaDisciplinasAlunoResponseDTO(MatriculaAluno matriculaAluno){
+        DisciplinasAlunoResponseDTO response = new DisciplinasAlunoResponseDTO();
+        response.setNomeDisciplina(matriculaAluno.getDisciplina().getNome());
+        response.setNomeDisciplina(matriculaAluno.getDisciplina().getProfessor().getNome());
+        response.setNota1(matriculaAluno.getNota1());
+        response.setNota2(matriculaAluno.getNota2());
+        response.setMedia(calcularMedia(matriculaAluno.getNota1(),matriculaAluno.getNota2());
+        response.setStatus(matriculaAluno);
+    }
+    private Double calcularMedia(Double nota1, Double nota2){
+        return (nota1 != null && nota2 != = null) ? (nota1 +nota2) / QTD_NOTAS : null;
+    }
+
 
 }
 
